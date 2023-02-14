803b2e6f02f23c49b49a5f5282349da10686d261
Kamronbek <60217990+nkr413@users.noreply.github.com>
2023-02-15 02:53:01 +0500

    add: struct, impl & some changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-	println!("Hello, world!");
+#![allow(warnings)]
+
+// MODULES
+mod keyboard;
+pub use crate::keyboard::keyboard_md;
+
+
+pub struct Main
+{
+	board: [[i64; 4]; 4],
+}
+
+impl Main
+{
+	fn up(&self)
+	{
+		println!("up");
+	}
+
+
+	fn left(&self)
+	{
+		println!("left");
+	}
+
+
+	fn down(&self)
+	{
+		println!("down");
+	}
+
+
+	fn right(&self)
+	{
+		println!("right");
+	}
+}
+
+
+fn main()
+{
+	let main = Main {
+ 		board: [
+ 			[0, 0, 0, 0],
+ 			[0, 0, 0, 0],
+ 			[0, 0, 0, 0],
+ 			[0, 0, 0, 0],
+ 		],
+ 	};
+
+ 	keyboard_md::start(&main);
+
+ 	//println!("{:?}", main.board);
 }
